8ec257a177adcfa447f14485e517f300218cb0b6
eeryczeek <eryk.ptaszynski@vp.pl>
2023-06-12 21:02:26 +0200

    we're getting closer to final version

--- interactive_dashboard/POKEMONS/server.R
+++ interactive_dashboard/POKEMONS/server.R
@@ -8,10 +8,16 @@ library(tidyr)
 library(treemap)
 
 function(input, output, session) {
-    set.seed(23)
-
-    # load pokemon data and fill in missing type 2 with type 1
     pokemon_data <- read.csv("data/Pokemon.csv")
+    
+    selected_pokemons <- reactiveVal(pokemon_data[1, ]$Name)
+    selected_type <- reactiveVal(NULL)
+    
+    View(selected_pokemons())
+    
+    observeEvent(input$show_pokemon_data_rows_selected, {
+        selected_pokemons(input$show_pokemon_data_rows_selected)
+    })
 
     #custom colors for each pokemon type
     pokemon_colors <- c(
@@ -35,22 +41,10 @@ function(input, output, session) {
         "Water" = "#6390F0"
     )
 
-    # find the min and max values for each stat
-    min_max <- pokemon_data %>%
-        select(Attack, Defense, Sp..Atk, Sp..Def, Speed) %>%
-        gather(key = "stat", value = "value") %>%
-        group_by(stat) %>%
-        summarise(min = min(value), max = max(value))
-
-    # Create a reactive values to store the selected pokemon and selected type
-    selected_pokemons <- reactiveVal(NULL)
-    selected_type <- reactiveVal(NULL)
-
-    # Show datatable of pokemon_names with vertical scrolling get rid of the bottom description
     output$show_pokemon_data <- DT::renderDataTable({
         DT::datatable(data.frame(pokemon_data$Name),
             options = list(scrollY = 200,
-                pageLength = 10,
+                pageLength = 80,
                 lengthChange = FALSE,
                 info = FALSE,
                 colnames = "Pokemon Name",
@@ -58,74 +52,51 @@ function(input, output, session) {
             selection = list(mode = "single", target = "row"),
             rownames = FALSE)
     })
-
-    # Update the selected row when a row is clicked
-    observeEvent(input$show_pokemon_data_rows_selected, {
-        selected_pokemons(input$show_pokemon_data_rows_selected)
+    
+    output$put_image <- renderImage({
+        put_image <- list(src = "logo.png",
+                              contentType = "image/png",
+                              width = 300,
+                              height = 80,
+                              alt = "Pokemon Image")
     })
 
-    # Update the plot title dynamically based on the selected Pokemon
+    output$pokemon_image <- renderImage({
+        pokemon_image <- list(src = paste0("data/images/", tolower(pokemon_data[selected_pokemons(), ]$Name), ".png"),
+                              contentType = "image/png",
+                              width = 256,
+                              height = 256,
+                              alt = "Pokemon Image")
+    })
+
     output$plot_title <- renderUI({
         HTML(paste0("<h3>", pokemon_data[selected_pokemons(), ]$Name, "</h3>"))
     })
 
-    # Plot the stats of selected pokemons on a radar chart fill inside add dynamic title and legend
-    output$pokemon_plot <- renderPlot({
-        if (is.null(selected_pokemons())) {
-            return(NULL)
-        }
+    output$radar_chart <- renderPlot({
         row <- pokemon_data[selected_pokemons(), ] %>%
             select(HP, Defense, Attack, Sp..Atk, Sp..Def, Speed)
-        row <- rbind(rep(250, 6), rep(0, 6), row)
+        row <- rbind(rep(255, 6), rep(0, 6), row)
         radarchart(
             row, axistype = 1,
-            cglcol = "grey", cglty = 1, axislabcol = "grey",
-            caxislabels = seq(0, 250, 6), cglwd = 0.8, vlcex = 0.8,
+            cglcol = "grey", cglty = 1, axislabcol = "grey", plwd = 3, plty = 1,
+            caxislabels = c(0, 50, 100, 150, 200, 250), cglwd = 0.8, vlcex = 0.8,
+            pfcol = scales::alpha(pokemon_colors[pokemon_data[selected_pokemons(), ]$Type.1], 0.5),
+            pcol = pokemon_colors[pokemon_data[selected_pokemons(), ]$Type.1],
             title = pokemon_data[selected_pokemons(), ]$Name,
         )
     })
 
-    # treemap of pokemon types
-    output$treemap <- renderPlot({
-        pokemon_data %>%
-            group_by(Type.1, Type.2) %>%
-            summarise(count = n()) %>%
-            mutate(
-                Type.1 = fct_reorder(Type.1, count),
-                Type.2 = fct_reorder(Type.2, count)
-            ) %>%
-            treemap(index = c("Type.1", "Type.2"), vSize = "count", title = "pokemon type distribution", palette = pokemon_colors)
-    })
-
-    # interactive treemap of pokemon types without using treemap package
     output$interactive_treemap <- renderPlotly({
         pokemon_data_treemap <- pokemon_data %>%
             group_by(Type.1, Type.2) %>%
-            summarise(count = n())
-        
-        # if Type.2 is blank, then swap Type.1 places of Type.2
-        pokemon_data_treemap <- pokemon_data_treemap %>%
+            summarise(count = n()) %>%
             mutate(Type.2 = ifelse(Type.2 == "", Type.1, Type.2)) %>%
-            mutate(Type.1 = ifelse(Type.2 == Type.1, "", Type.1))
-        
-        # change name of Type.1 and Type.2 to label and parent
-        pokemon_data_treemap <- pokemon_data_treemap %>%
-            rename(label = Type.2, parent = Type.1)
-        
-        # change label to parent + label if parent is not blank
-        pokemon_data_treemap <- pokemon_data_treemap %>%
+            mutate(Type.1 = ifelse(Type.2 == Type.1, "", Type.1)) %>%
+            rename(label = Type.2, parent = Type.1) %>%
             mutate(label = ifelse(parent == "", label, paste(parent, label, sep = " - ")))
-
-        # Create an event handler for treemap click event
-        event_data <- event_data("plotly_click", source = "interactive_treemap")
-
-        if (!is.null(event_data)) {
-            # Get the selected label (primary_pokemon_type) from the clicked part
-            selected_label <- event_data[["x"]]
-            selected_type(selected_label)
-        }
-
-        pokemon_data_treemap %>%
+        
+        plot <- pokemon_data_treemap %>%
             plot_ly(
                 labels = ~label,
                 parents = ~parent,
@@ -138,45 +109,26 @@ function(input, output, session) {
                     "Count: %{value}<br>"
                 )
             )
+        plot <- event_register(plot, "plotly_click")
+        plot
+    })
+    
+    observeEvent(event_data("plotly_click", source = "interactive_treemap"), {
+        selected_label <- event_data("plotly_click", source = "interactive_treemap")$label
+        if (selected_label == selected_type()) {
+            selected_type(NULL)
+        } else {
+            selected_type(selected_label)
+        }
     })
 
-    # pokemon_image
-    output$pokemon_image <- renderImage({
-            if (is.null(selected_pokemons())) {
-                return(NULL)
-            }
-            selected_pokemon_data <- pokemon_data[selected_pokemons(), ]
-            name <- selected_pokemon_data$Name
-            # return the image from archive/images/images directory
-            pokemon_image <- list(src = paste0("data/images/", tolower(name), ".png"),
-                                  contentType = "image/png",
-                                  width = 256,
-                                  height = 256,
-                                  alt = "Pokemon Image")
-            return(pokemon_image)
-        },
-        deleteFile = FALSE
-    )
-
-    # violin plot of pokemon stats
     output$violin_plot <- renderPlotly({
-    if (is.null(selected_type())) {
-        return(
-            # violin plot for all the pokemons for all the stats
-            pokemon_data %>%
-                select(HP, Defense, Attack, Sp..Atk, Sp..Def, Speed) %>%
-                gather(key = "stat", value = "value") %>%
-                ggplot(aes(x = stat, y = value, fill = stat)) +
-                geom_violin() +
-                geom_boxplot(width = 0.1, fill = "white", color = "black") +
-                theme_minimal() +
-                theme(legend.position = "none") +
-                labs(x = "Stat", y = "Value", title = "Pokemon Stats")
-        )
-    } else {
-        filtered_data <- pokemon_data %>%
-            filter(Type.1 == selected_type() | Type.2 == selected_type())
-
+        if (!is.null(selected_type())) {
+            filtered_data <- pokemon_data %>%
+                filter(Type.1 == selected_type() | Type.2 == selected_type())
+        } else {
+            filtered_data <- pokemon_data
+        }
         filtered_data %>%
             select(HP, Defense, Attack, Sp..Atk, Sp..Def, Speed) %>%
             gather(key = "stat", value = "value") %>%
@@ -186,7 +138,21 @@ function(input, output, session) {
             theme_minimal() +
             theme(legend.position = "none") +
             labs(x = "Stat", y = "Value", title = "Pokemon Stats")
-    }
-})
+    })
 
+    # contour plot for selected type of pokemon
+    output$contour_plot <- renderPlotly({
+        if (!is.null(selected_type())) {
+            filtered_data <- pokemon_data %>%
+                filter(Type.1 == selected_type() | Type.2 == selected_type())
+        } else {
+            filtered_data <- pokemon_data
+        }
+        filtered_data %>%
+            ggplot(aes(x = Attack, y = Defense)) +
+            geom_density2d() +
+            geom_point(alpha = 0.1) +
+            theme_minimal() +
+            labs(x = "Attack", y = "Defense", title = "Pokemon Attack vs Defense")
+    })
 }
--- interactive_dashboard/POKEMONS/ui.R
+++ interactive_dashboard/POKEMONS/ui.R
@@ -1,39 +1,85 @@
 library(shiny)
 library(shinydashboard)
-library(DT)
 library(plotly)
 
 dashboardPage(
-    dashboardHeader(title = "Pokemon Dashboard"),
-    dashboardSidebar(disable = TRUE),
+    dashboardHeader(
+        title = "Pokemon Dashboard",
+        titleWidth = 300,
+        tags$li(
+            class = "dropdown",
+            tags$a(
+                href = "#",
+                class = "dropdown-toggle",
+                `data-toggle` = "dropdown",
+                tags$i(class = "fa fa-question-circle"),
+                style = "color: #fff;"
+            ),
+            tags$ul(
+                class = "dropdown-menu",
+                style = "min-width: 300px;",
+                tags$li(
+                    tags$div(
+                        style = "padding: 10px;",
+                        "This is a Pokemon Dashboard where you can explore various information about Pokemon.",
+                        tags$p("Here are some features of this dashboard:"),
+                        tags$ul(
+                            tags$li("The 'Pokemon Name' table displays the list of Pokemon names. Selecting a row highlights the corresponding Pokemon."),
+                            tags$li("The 'Radar Chart' shows the stats (HP, Defense, Attack, Sp..Atk, Sp..Def, Speed) of the selected Pokemon."),
+                            tags$li("The 'Interactive Treemap' provides a hierarchical view of the Pokemon types. Click on a type to filter the data."),
+                            tags$li("The 'Pokemon Image' displays the image of the selected Pokemon."),
+                            tags$li("The 'Violin Plot' visualizes the distribution of stats for all Pokemon or the selected type."),
+                            tags$li("The 'Contour Plot' shows the relationship between Attack and Defense stats for all Pokemon or the selected type.")
+                        )
+                    )
+                )
+            )
+        )
+    ),
+    dashboardSidebar(
+        disable = TRUE),
     dashboardBody(
         fluidRow(
             box(
-                width = 3,
+                width = 2,
                 DT::dataTableOutput("show_pokemon_data")
             ),
+            # make a column with logo image and them imageOutput of pokemon
+            column(
+                width = 3,
+                box(
+                    width = NULL,
+                    height = "120px",
+                    plotOutput("put_image")
+                ),
+                box(
+                    height = "280px",
+                    width = NULL,
+                    plotOutput("pokemon_image")
+                )
+            ),
             box(
-                status = "warning", width = 6,
-                imageOutput("pokemon_image")
+                width = 4,
+                plotOutput("radar_chart")
             ),
             box(
                 width = 3,
-                plotOutput("pokemon_plot")
+                plotlyOutput("contour_plot")
             )
         ),
         fluidRow(
             column(
                 width = 7,
                 box(
-                    width = NULL, solidHeader = TRUE, status = "primary",
+                    width = NULL,
                     plotlyOutput("interactive_treemap")
                 ),
             ),
             column(
                 width = 5,
                 box(
-                    width = NULL, solidHeader = TRUE,
-                    plotOutput("violin_plot")
+                    width = NULL,
+                    plotlyOutput("violin_plot")
                 ),
             )
         )
